4b8828b741bd0b8efd67e3cb2c49dc0f9a9f8efd
malfoy <antoine.limasset@gmail.com>
2021-07-01 12:23:24 +0200

    freddie now use a sparsemap

--- freddie.cpp
+++ freddie.cpp
@@ -1,5 +1,6 @@
 #include "robin_hood.h"
 #include "strict_fstream.hpp"
+#include "sparse_map.h"
 #include "zstr.hpp"
 #include <algorithm>
 #include <array>
@@ -40,7 +41,7 @@ template <>
 
 typedef uint8_t color;
 // key = kmer, value = pair (color: genomes where the kmer occurs. Max 8 genomes, boolean: has been seen (for feeding venn diagrams))
-typedef robin_hood::unordered_flat_map<kmer, pair<color,bool>> Map;
+typedef tsl::sparse_map<kmer, pair<color,bool>> Map;
 
 
 
